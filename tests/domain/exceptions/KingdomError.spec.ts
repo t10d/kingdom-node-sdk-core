1ac56041f28abd1b7d19d10b6b54b10a52dac86a
William Abreu <william@t10.digital>
2022-03-03 08:54:27 -0300

    tests: added error handling unit test

---
+++ tests/domain/exceptions/KingdomError.spec.ts
@@ -0,0 +1,20 @@
+import KingdomError from '../../../src/domain/exceptions/KingdomError';
+
+class MyError extends KingdomError {}
+
+test('Test error throwing', () => {
+  const { name } = MyError;
+  const message = 'Test Error';
+  const code = 'TEST_ERROR';
+
+  try {
+    throw new MyError(message, code);
+  } catch (error) {
+    expect(error).toBeInstanceOf(KingdomError);
+    expect(error.name).toEqual(name);
+    expect(error.message).toEqual(message);
+    expect(error.code).toEqual(code);
+    expect(error.toString()).toEqual(`${name}: ${message} [${code}]`);
+    expect(error.stack).toContain(`${name}: ${message}`);
+  }
+});
