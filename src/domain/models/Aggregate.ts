55cfe0ab510e26967e1787d3a913db9c3b339980
William Abreu <william@t10.digital>
2022-03-03 15:26:20 -0300

    feat: added aggregate modeling

---
+++ src/domain/models/Aggregate.ts
@@ -0,0 +1,33 @@
+import { AnyEvent } from '../aliases';
+import Entity from './Entity';
+
+/**
+ * Base class for aggregates.
+ */
+export default abstract class Aggregate<Id_T> extends Entity<Id_T> {
+  private _events: AnyEvent[];
+
+  public constructor(
+    id: Id_T,
+    version: number,
+    isDiscarded: boolean,
+    registeredAt: Date,
+    updatedAt: Date,
+  ) {
+    super(id, version, isDiscarded, registeredAt, updatedAt);
+    this._events = [];
+  }
+
+  public addEvents(...events: AnyEvent[]) {
+    this.check_not_discarded();
+    this._events.push(...events);
+  }
+
+  public hasEvents(): boolean {
+    return this._events.length > 0;
+  }
+
+  public nextEvent(): AnyEvent | undefined {
+    return this._events.shift();
+  }
+}
